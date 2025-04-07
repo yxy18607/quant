33ea08ac511fbab8cdf50f171a943f83a88ece01
Shawn Yu <15618592893@163.com>
2025-04-07 18:04:39 +0800

    beta择时调用脚本优化

--- BetaTest.py
+++ BetaTest.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 from BetaModules import DailyCTA
 
-cfg = {'startdate': '20180101',
-       'enddate': '20250228',
+cfg = {'startdate': '20230101',
+       'enddate': '20250331',
        'signal_id': 'timing5',
        # 'zone': 'hk',
        'trade_price': 'open',
@@ -17,5 +17,5 @@ backtest = DailyCTA(cfg)
 backtest()
 # backtest.get_pnl()
 # backtest.profit_ana()
-backtest.plot_curve(os_startdate='20230101')
+# backtest.plot_curve(os_startdate='20230101')
 # backtest.plot_curve()
