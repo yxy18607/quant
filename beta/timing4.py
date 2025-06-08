dcbb2456ac0f6bc804f33c9de257d9e48947bcd7
yxy <466472797@qq.com>
2025-06-09 02:14:39 +0800

    更新相关性检测方法

--- beta/timing4.py
+++ beta/timing4.py
@@ -12,6 +12,7 @@ class timing4(Beta):
     def generate_beta(self, df):
         window = 6
         ret = (df['high']+df['low']-(df['close']+df['open'])).values; volume = df['volume'].values
+        skew = df['low'].rolling(5).skew().values
         signal = np.full(len(df), 0)
         for i in range(window-1, len(df)):
             subret = ret[i-window+1:i+1]
@@ -20,8 +21,10 @@ class timing4(Beta):
             if ret[i]>=0 and upret/dnret>=1:
                 signal[i] = 1
             else:
-                if ret[i-1:i+1].sum()>0 and volume[i]>volume[i-1]:
+                # if ret[i-1:i+1].sum()>0 and volume[i]>volume[i-1]:
+                if ret[i-1:i+1].sum()>0 and skew[i]>0:
                     signal[i] = 1
-                elif ret[i-1:i+1].sum()<0 and volume[i]<volume[i-1]:
+                # elif ret[i-1:i+1].sum()<0 and volume[i]<volume[i-1]:
+                elif ret[i-1:i+1].sum()<0 and skew[i]<0:
                     signal[i] = -1
         return signal
