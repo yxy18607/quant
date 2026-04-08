be80daa5ab9f998f39ab9f38963cb66cf3483262
Shawn Yu <15618592893@163.com>
2026-04-08 15:02:25 +0800

    策略更新

--- beta/timing6.py
+++ beta/timing6.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import sys
+import FastRolling as fr
+import polars as pl
+from datetime import time
 
 sys.path.append('.')
 from BetaModules import Beta
@@ -9,21 +12,22 @@ class timing6(Beta):
     def __init__(self, cfg):
         super().__init__(cfg)
         
-    def generate_beta(self, df):
-        open = df['open'].values; close = df['close'].values; high = df['high'].values; low = df['low'].values
-        # f4 = df['pct_change']
+    def generate_signal(self, df):
+        open = df['open'].values; close = df['close'].values; high = df['high'].values; low = df['low'].values; vol = df['volume'].values
         signal = np.full(len(df), 0)
-        for i in range(1, len(df)):
-            trd_max = np.maximum(close[i], open[i]); trd_min = np.minimum(close[i], open[i])
-            exp_u = high[i]-trd_max; exp_d = trd_min-low[i]
-            if exp_u>exp_d:
-                if open[i]<close[i-1]:
-                    signal[i] = 1
-                elif close[i]<open[i]:
-                    signal[i] = 1
-            else:
-                if open[i]>close[i-1]:
-                    signal[i] = -1
-                elif high[i]<high[i-1]:
-                    signal[i] = -1
+        skd = df['skew'].values
+        for i in range(2, len(df)):
+            if skd[i]>skd[i-1] and high[i]>high[i-1]:
+                signal[i] = 1
+            elif skd[i]<skd[i-1] and high[i]<high[i-1]:
+                signal[i] = -1
         return signal
+    
+    def intra_to_daily(self, pf: pl.LazyFrame):
+        pf = (pf
+              .with_columns([pl.col('time_id').dt.time().alias('time'),
+                             (pl.col('close')/pl.col('pre')-1).alias('ret')])
+              .group_by('date_id').agg([pl.col('close').skew().alias('skew'),
+              ])
+)
+        return pf
--- beta/timingc_v2.py
+++ beta/timingc_v2.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('.')
 from BetaModules import Beta
 
-class timingc_v1(Beta):
+class timingc_v2(Beta):
     def __init__(self, cfg):
         super().__init__(cfg)
         
@@ -15,5 +15,6 @@ class timingc_v1(Beta):
         factor3 = pd.read_pickle('./dump/timing3.pkl')
         factor4 = pd.read_pickle('./dump/timing4.pkl')
         factor5 = pd.read_pickle('./dump/timing5.pkl')
-        beta = (factor1+factor2+factor3+factor4+factor5)/5
-        self.signal_df = beta.apply(lambda col: pd.cut(col, bins=[-1, -0.6, -0.2, 0.1, 0.5, 1], labels=[-1, -0.5, 0, 0.5, 1], include_lowest=True)).astype(float)
+        factor6 = pd.read_pickle('./dump/timing6.pkl')
+        beta = (factor1 + factor2 + factor3 + factor4 + factor5 + factor6)/6
+        self.signal_df = beta.apply(lambda col: pd.cut(col, bins=[-1, -0.6, -0.2, 0.2, 0.6, 1], labels=[-1, -0.5, 0, 0.5, 1], include_lowest=True)).astype(float)
