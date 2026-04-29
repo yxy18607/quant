9dfb30bf315f73c8681b86deccca28f5f5d8cfd8
Shawn Yu <15618592893@163.com>
2026-04-29 17:34:14 +0800

    timing7+

--- beta/timing7.py
+++ beta/timing7.py
@@ -12,21 +12,24 @@ class timing7(Beta):
     def __init__(self, cfg):
         super().__init__(cfg)
         
-    def generate_beta(self, df):
-        open = df['open'].values; close = df['close'].values; high = df['high'].values; low = df['low'].values
-        openr = df['openr'].values
+    def generate_signal(self, df):
         signal = np.full(len(df), 0)
-        for i in range(len(df)):
-            if openr[i]>0:
-                signal[i] = 1
-            else:
+        closerev = df['closeret'].values
+        profit_ratio = df['closeret'].abs()/df['pct_change'].abs()
+        profit_rank = fr.signal_uniform(profit_ratio, 5).values
+        fear_rank = fr.signal_uniform(df['closevol'], 5).values
+        for i in range(4, len(df)):
+            if closerev[i]>0 and profit_rank[i]>0:
                 signal[i] = -1
+            elif closerev[i]<0 and fear_rank[i]>0:
+                signal[i] = 1
         return signal
     
-    def pf_to_daily(self, pf: pl.LazyFrame):
+    def intra_to_daily(self, pf: pl.LazyFrame):
         pf = (pf
               .with_columns([pl.col('time_id').dt.time().alias('time'),
                              (pl.col('close')/pl.col('pre')-1).alias('ret')])
-              .group_by('date_id').agg(pl.when(pl.col('time')<=time(10, 0)).then(pl.col('ret')).otherwise(None).sum().alias('openr'))
-              )
+              .group_by('date_id').agg([pl.col('ret').filter(pl.col('time')>=time(14, 30)).sum().alias('closeret'),
+                                        pl.col('volume').filter(pl.col('time')>=time(14, 30)).sum().alias('closevol')])
+)
         return pf
---
+++ beta/timingc_v3.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import sys
+
+sys.path.append('.')
+from BetaModules import Beta
+
+class timingc_v3(Beta):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        
+    def process(self):
+        factor1 = pd.read_pickle('./dump/timing1.pkl')
+        factor2 = pd.read_pickle('./dump/timing2.pkl')
+        factor3 = pd.read_pickle('./dump/timing3.pkl')
+        factor4 = pd.read_pickle('./dump/timing4.pkl')
+        factor5 = pd.read_pickle('./dump/timing5.pkl')
+        factor6 = pd.read_pickle('./dump/timing6.pkl')
+        factor7 = pd.read_pickle('./dump/timing7.pkl')
+        beta = (factor1+factor2 + factor3 + factor4 + factor5 + factor6 + factor7)/7
+        self.signal_df = beta.apply(lambda col: pd.cut(col, bins=[-1, -0.5, -0.1, 0.1, 0.5, 1], labels=[-1, -0.5, 0, 0.5, 1], include_lowest=True)).astype(float)
